533eb6e80144c0c4ca510ff37ad62b20dabc5f73
mingchaoyan <mingchaoyan@gmail.com>
2014-09-23 11:49:05 +0800

    添加未track的文件

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    public Transform m_transform;
+    CharacterController m_ch;
+
+    float m_movSpeed = 3.0f;
+    float m_gravity = 2.0f;
+    public int m_life = 5;
+
+    Transform m_camTransform;
+    Vector3 m_camRot;
+    float m_camHeight = 1.4f;
+
+	// Use this for initialization
+	void Start () {
+        m_transform = this.transform;
+        m_ch = this.GetComponent<CharacterController>();
+
+        m_camTransform = Camera.main.transform;
+        Vector3 pos = m_transform.position;
+        pos.y += m_camHeight;
+        m_camTransform.position = pos;
+
+        m_camTransform.rotation = m_transform.rotation;
+        m_camRot = m_camTransform.eulerAngles;
+
+        Screen.lockCursor = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_life <= 0)
+            return;
+        Control();
+	}
+
+    void Control()
+    {
+        float rh = Input.GetAxis("Mouse X");
+        float rv = Input.GetAxis("Mouse Y");
+        m_camRot.x -= rv;
+        m_camRot.y += rh;
+        m_camTransform.eulerAngles = m_camRot;
+
+        Vector3 camrot = m_camTransform.eulerAngles;
+        camrot.x = 0;
+        camrot.z = 0;
+        m_transform.eulerAngles = camrot;
+
+        float xm = 0, ym = 0, zm = 0;
+        ym -= m_gravity * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            zm += m_movSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            zm -= m_movSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            xm -= m_movSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            xm += m_movSpeed * Time.deltaTime;
+        }
+        m_ch.Move(m_transform.TransformDirection(new Vector3(xm, ym, zm)));
+        Vector3 pos = m_transform.position;
+        pos.y += m_camHeight;
+        m_camTransform.position = pos;
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(this.transform.position, "Spawn.tif");
+    }
+}
